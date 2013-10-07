bac403619bae7df28bca1fda7988ce2b861f1f31
JFingert <joshuafingert@gmail.com>
2013-10-07 10:09:41 -0700

    Yo World!

---
+++ variables.js
@@ -0,0 +1,34 @@
+Joshua Fingert
+//the marbles
+
+var marble1 = "Blue"
+var marble2 = "Red"
+var marble3 = "Yellow"
+
+var allMarbles = marble1 + " " + marble2 + " " + marble3 + "!"
+
+console.log(allMarbles)
+
+
+//the numbers
+
+var one = 1
+var two = 2
+var three = 3
+
+var four = (three+two-one)
+
+console.log(four)
+
+(5*4)+10-4
+(5*4)+(10-4)
+(10-4)+5*4
+var n = 10-4+5*4
+
+console.log(n)
+
+//string and number concatenation
+
+var zzz = marble1+one+2
+
+console.log(zzz)
